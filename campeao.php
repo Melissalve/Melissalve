c4b65608bb64a90e9bcf2345a8977d22556351a9
Melissalve <105174585+Melissalve@users.noreply.github.com>
2022-05-08 19:26:35 -0300

    Create campeao.php

---
+++ campeao.php
@@ -0,0 +1,31 @@
+<?php
+print("Quem é o campeão")
+  renato = 67;
+  ana = 45;
+  lucas = 89;
+  maria = 56;
+
+print("Renato, 67 votos");
+print("Ana, 45 votos");
+print("Lucas, 89 votos");
+print("Maria, 56 votos");
+
+if ana < renato:
+  print("Renato é o campeão");
+else:
+  print("Ana não é campeã");
+if lucas > renato:
+  print("Lucas é o campeão");
+else:
+  print("Renato não é o campeão");
+if lucas > maria:
+  print("Lucas é o campeão");
+else:
+  print("Maria não é campeã");
+if ana < lucas;
+  print("Lucas é o campeão");
+else:
+  print("Ana não é campeã");
+
+
+?>
