c0a54c0b9b7942d2443f8f7496cf5e94eb0cc21a
Melissalve <105174585+Melissalve@users.noreply.github.com>
2022-05-08 18:58:06 -0300

    Create Boletim.php

---
+++ Boletim.php
@@ -0,0 +1,13 @@
+<?php
+aluno1 = input ("Melissa");
+nt1 = float(inpunt("8");
+nt2 = float(inpunt("7");
+nt3 = float(inpunt("10");
+faltas = int(inpunt("6");
+media = (("Média:7", round(media));
+if media >=7 and faltas < 20;
+  print("Aluno:Melissa ", aluno, "Aprovado");
+else:
+  print("Aluno:Melissa", aluno, "Reprovado");
+
+?>
