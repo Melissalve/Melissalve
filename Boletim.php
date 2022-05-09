0ad9b2829f42fc8a668753929468183b60487fcb
Melissalve <105174585+Melissalve@users.noreply.github.com>
2022-05-09 14:01:21 -0400

    Update Boletim.php

--- Boletim.php
+++ Boletim.php
@@ -1,13 +1,13 @@
 <?php
-aluno1 = input ("Melissa");
-nt1 = float(inpunt("8");
-nt2 = float(inpunt("7");
-nt3 = float(inpunt("10");
-faltas = int(inpunt("6");
+aluno1 = input ("digite o nome do aluno");
+nt1 = float(inpunt("Digite 1 nota");
+nt2 = float(inpunt("Digite 2 nota");
+nt3 = float(inpunt("Digite 3 nota");
+faltas = int(inpunt("Digite as faltas do aluno");
 media = (("Média:7", round(media));
 if media >=7 and faltas < 20;
-  print("Aluno:Melissa ", aluno, "Aprovado");
+  print("Aluno: ", aluno1, "Aprovado");
 else:
-  print("Aluno:Melissa", aluno, "Reprovado");
+  print("Aluno:", aluno1, "Reprovado");
 
 ?>
